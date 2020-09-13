10208b312882456d03c664d7a9c49e85c31cb933
Marcelo11114 <dedosearte@hotmail.com>
2020-09-13 14:08:53 -0300

    Letra de musica - Valdecir Marcelo ra11114 - pc1

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace LetraMusica
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("***************");
+            Console.WriteLine("* Deixe-me ir *");
+            Console.WriteLine("***************\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Preciso andar");
+            Console.WriteLine("Vou por aí a procurar");
+            Console.WriteLine("Rir pra não chorar");
+            Console.WriteLine("Deixe-me ir");
+            Console.WriteLine("Preciso andar");
+            Console.WriteLine("Vou por aí a procurar");
+            Console.WriteLine("Sorrir pra não chorar\n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Quero assistir ao sol nascer");
+            Console.WriteLine("Ver as águas dos rios correr");
+            Console.WriteLine("Ouvir os pássaros cantar");
+            Console.WriteLine("Eu quero nascer");
+            Console.WriteLine("Quero viver\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Deixe-me ir");
+            Console.WriteLine("Preciso andar");
+            Console.WriteLine("Vou por aí a procurar");
+            Console.WriteLine("Rir pra não chorar\n");
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("Se alguém por mim perguntar");
+            Console.WriteLine("Diga que eu só vou voltar");
+            Console.WriteLine("Depois que me encontrar");
+            Console.WriteLine("Quero assistir ao sol nascer");
+            Console.WriteLine("Ver as águas dos rios correr");
+            Console.WriteLine("Eu quero nascer");
+            Console.WriteLine("Quero viver\n");
+            Console.ForegroundColor = ConsoleColor.Red;
+
+            Console.WriteLine("Deixe-me ir");
+            Console.WriteLine("Preciso andar");
+            Console.WriteLine("Vou por aí a procurar");
+            Console.WriteLine("Sorrir pra não chorar");
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\nCartola");
+            Console.WriteLine("*******");
+            Console.WriteLine("\nTecla qqr tecla para sair");
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+}
